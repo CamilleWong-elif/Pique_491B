d4056a7fe44689d5f47175b606c5ecc64124dcfc
anhdroid25 <159067754+anhdroid25@users.noreply.github.com>
2026-03-01 13:35:30 -0800

    working on referencing database

--- pique_491b/src/config/firebase.ts
+++ pique_491b/src/config/firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
@@ -11,5 +13,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 export default app;
