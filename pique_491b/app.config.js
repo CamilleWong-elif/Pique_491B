0052c119d7b8ddd7da270b5a5bffa3d59adfe69d
David <dhk704@gmail.com>
2026-02-28 18:05:38 -0800

    Revise for Android compatibility pt 1

--- pique_491b/app.config.js
+++ pique_491b/app.config.js
@@ -1,10 +1,23 @@
 require("dotenv").config();
 
-const { expo } = require("./app.json");
-
 module.exports = {
   expo: {
-    ...expo,
+    name: "PiqueApp",
+    slug: "piqueapp",
+    scheme: "piqueapp",
+    platforms: ["android"],
+    android: {
+      package: "com.piqueapp.main",
+      adaptiveIcon: {
+        foregroundImage: "./src/assets/temp_logo.png",
+        backgroundColor: "#2E96F0"
+      },
+      config: {
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
+        }
+      }
+    },
     extra: {
       firebase: {
         apiKey: process.env.FIREBASE_API_KEY,
