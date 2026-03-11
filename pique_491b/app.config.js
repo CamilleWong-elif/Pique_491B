9572d9c83e809dd4e81a8823da26d2ae91e3a31d
Camille Wong <camyw123@gmail.com>
2026-03-10 20:52:10 -0700

    fixed google signin merge conflicts

--- pique_491b/app.config.js
+++ pique_491b/app.config.js
@@ -6,7 +6,7 @@ module.exports = {
     slug: "piqueapp",
     owner: "camyw",
     scheme: "piqueapp",
-    platforms: ["android"],
+    platforms: ["android", "ios"],
     android: {
       package: "com.piqueapp.main",
       adaptiveIcon: {
@@ -19,20 +19,9 @@ module.exports = {
         },
       },
     },
-<<<<<<< HEAD
-    extra: {
-      firebase: {
-        apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-=======
     ios: {
       bundleIdentifier: "com.piqueapp.main",
     },
-    // ─── ADD THIS PLUGINS ARRAY ───
     plugins: [
       [
         "@react-native-google-signin/google-signin",
@@ -43,16 +32,15 @@ module.exports = {
     ],
     extra: {
       firebase: {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
+        apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
       },
       googleAuth: {
         webClientId: process.env.EXPO_PUBLIC_GOOGLE_EXPO_CLIENT_ID,
->>>>>>> 7504cb9 (fixed google signin)
       },
     },
   },
